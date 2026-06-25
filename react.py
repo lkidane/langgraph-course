b460c7554b8bee38d88f8e0e3e5ca7217b50c46f
lkidane <lkidane@aimsammi.org>
2026-06-25 16:34:04 +0200

    langraph react agent first

--- react.py
+++ react.py
@@ -2,5 +2,27 @@ from dotenv import load_dotenv
 from langchain_core.tools import tool
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
+from langchain_ollama import ChatOllama
 
 load_dotenv()
+
+
+import os
+print("TAVILY:", os.getenv("TAVILY_API_KEY"))
+
+@tool
+def triple(num:float) -> float:
+    """
+    param num: a number to triple
+    returns: the triple of the input number
+    """
+    return float(num) * 3
+
+tools = [TavilySearch(max_results=1), triple]
+
+# llm = ChatOpenAI(model="gpt-4o-mini", temperature=0).bind_tools(tools)
+
+llm = ChatOllama(
+    model="mymodel",   # or mistral, phi3, etc.
+    temperature=0
+).bind_tools(tools)
